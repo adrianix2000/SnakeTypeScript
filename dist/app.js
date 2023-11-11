9f51e175313c7c1969feb4dc75e9b7928d6e77eb
Adrian Sak <adrianvsak@gmail.com>
2023-11-12 00:52:23 +0100

    add GameRound class and implement detetecting keys pressed

--- dist/app.js
+++ dist/app.js
@@ -47,10 +47,55 @@ class SnakeMap {
         return this.mapContent;
     }
 }
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+class GameRound {
+    constructor(element) {
+        this.canvas = element;
+        this.map = new SnakeMap(20, 20);
+        this.drawMap();
+        this.setupKeyboardControls();
+        this.isStopped = true;
+        this.mainLoop();
+    }
+    mainLoop() {
+        while (this.isStopped) {
+        }
+    }
+    drawMap() {
+        this.canvas.innerHTML = this.map.getGeneratedMap();
+    }
+    setupKeyboardControls() {
+        document.addEventListener('keydown', (event) => {
+            switch (event.key) {
+                case 'ArrowUp':
+                    console.log("up");
+                    break;
+                case 'ArrowDown':
+                    console.log("down");
+                    break;
+                case 'ArrowLeft':
+                    console.log("left");
+                    break;
+                case 'ArrowRight':
+                    console.log("right");
+                    break;
+                case 'Escape':
+                    this.isStopped = !this.isStopped;
+                    console.log(this.isStopped);
+                    break;
+            }
+        });
+    }
+}
 document.addEventListener('DOMContentLoaded', () => {
     const appElement = document.getElementById('app');
     if (appElement) {
-        let map = new SnakeMap(20, 20);
-        appElement.innerHTML = map.getGeneratedMap();
+        let round = new GameRound(appElement);
     }
 });
--- src/app.ts
+++ src/app.ts
@@ -69,12 +69,68 @@ class SnakeMap {
     }
 }
 
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+class GameRound {
+    private map: SnakeMap;
+    private canvas: HTMLElement;
+    private isStopped: boolean;
+
+    constructor(element: HTMLElement) {
+        this.canvas = element;
+        this.map = new SnakeMap(20, 20);
+        this.drawMap();
+        this.setupKeyboardControls();
+        this.isStopped = true;
+        this.mainLoop();
+    }
+
+    mainLoop() {
+        while(this.isStopped) {
+
+        }
+    }
+
+    drawMap() {
+        this.canvas.innerHTML = this.map.getGeneratedMap();
+    }
+
+    private setupKeyboardControls() {
+        document.addEventListener('keydown', (event) => {
+            switch (event.key) {
+                case 'ArrowUp':
+                    console.log("up");
+                    break;
+                case 'ArrowDown':
+                    console.log("down");
+                    break;
+                case 'ArrowLeft':
+                    console.log("left");
+                    break;
+                case 'ArrowRight':
+                    console.log("right");
+                    break;
+                case 'Escape':
+                    this.isStopped = !this.isStopped;
+                    console.log(this.isStopped);
+                    break;
+            }
+        });
+    }
+
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const appElement: HTMLElement | null = document.getElementById('app');
 
     if (appElement) {
-        let map: SnakeMap = new SnakeMap(20, 20);
-        appElement.innerHTML = map.getGeneratedMap();
+        let round: GameRound = new GameRound(appElement); 
     }
 });
 
